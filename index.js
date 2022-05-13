ffe935a9772376fe27fd669e0698986997976e21
Erwin <erwin.stein@gmail.com>
2022-05-13 00:08:43 -0300

    12/05/22 - 00:01

--- index.js
+++ index.js
@@ -1,14 +1,99 @@
-// VARIAVEIS
-var btnMenu = document.querySelector("#ico-config");
+// SELETORES
+var iconeMenu = document.querySelector("#ico-config");
+var iconeDark = document.querySelector("#ico-dark");
+var iconeLight = document.querySelector("#ico-light");
 var configModal = document.querySelector(".modal");
-var container = document.querySelector(".grid-imagem");
+var container = document.querySelectorAll(".aux-modal");
 
 
 // MOSTRA / ESCONDE MODAL 
-btnMenu.addEventListener("click", function () {
+iconeMenu.addEventListener("click", function () {
 	configModal.classList.toggle("hide");
 });
 // ESCONDE MODAL NO CLICK FORA
-container.addEventListener("click", function(){
-	configModal.classList.add("hide");
+container.forEach(function (item, idx) {
+    item.addEventListener('click', function () {
+        configModal.classList.add("hide");
+    });
+});
+
+window.onload = functionAddAttribute;
+// SELETORES E CORES THEMA
+var cor_azul = document.getElementById('cor_azul');
+var cor_vermelho = document.getElementById('cor_vermelho');
+var cor_verde = document.getElementById('cor_verde');
+var icones_svg = document.querySelectorAll('.icones_svg');
+
+var bgLight = "242,242,242";
+var bgDark = "30,30,30";
+
+var corAzul ="92,122,255"; /*AZUL - 5c7aff */
+var corVerde = "3,181,170"; /*VERDE - 03b5aa */
+var corVermelho = "251,80,18"; /*VERMELHO - fb5012 */
+
+var SVGAZUL = "invert(50%) sepia(37%) saturate(5876%) hue-rotate(213deg) brightness(101%) contrast(101%)";
+var SVGVERDE ="invert(49%) sepia(87%) saturate(471%) hue-rotate(127deg) brightness(94%) contrast(98%)";
+var SVGVERMELHO = "invert(41%) sepia(68%) saturate(4148%) hue-rotate(354deg) brightness(100%) contrast(98%)";
+
+
+var corPadrao = sessionStorage.getItem('corPadrao');
+var bgPadrao = sessionStorage.getItem('bgPadrao');
+var svgPadrao = sessionStorage.getItem('svgPadrao');
+
+// ONLOAD ATRIBUTE
+function functionAddAttribute(){
+    document.documentElement.style.setProperty("--bg-global", bgPadrao);
+    document.documentElement.style.setProperty("--cor-global", corPadrao);
+    document.documentElement.style.setProperty("--cor-svg-global", svgPadrao);
+};
+
+// CLICK ICONE DARK
+iconeDark.addEventListener("click", function(){
+    document.documentElement.style.setProperty("--bg-global", bgDark);
+    sessionStorage.setItem("bgPadrao", bgDark);
+
+});
+// CLICK ICONE LIGHT
+iconeLight.addEventListener("click", function(){
+    document.documentElement.style.setProperty("--bg-global", bgLight);
+    sessionStorage.setItem("bgPadrao", bgLight);    
+});
+// MUDA PARA COR AZUL
+cor_azul.addEventListener("click", function() {
+    document.documentElement.style.setProperty("--cor-global", corAzul);
+    sessionStorage.setItem("corPadrao", corAzul);
+    sessionStorage.setItem("svgPadrao", SVGAZUL);    
+    configModal.classList.add("hide");
+
+    for(var i = 0; i < icones_svg.length; i++) {
+    icones_svg[i].classList.add('svg-azul');
+    icones_svg[i].classList.remove('svg-verde');
+    icones_svg[i].classList.remove('svg-vermelho');
+    }   
+});
+// MUDA PARA COR VERDE
+cor_verde.addEventListener("click", function() {
+    document.documentElement.style.setProperty("--cor-global", corVerde);
+    sessionStorage.setItem("corPadrao", corVerde);
+    sessionStorage.setItem("svgPadrao", SVGVERDE); 
+    configModal.classList.add("hide");
+
+    for(var i = 0; i < icones_svg.length; i++) {
+    icones_svg[i].classList.remove('svg-azul');
+    icones_svg[i].classList.add('svg-verde');
+    icones_svg[i].classList.remove('svg-vermelho');
+    }
+});
+// MUDA PARA COR VERMELHA
+cor_vermelho.addEventListener("click", function() {
+    document.documentElement.style.setProperty("--cor-global", corVermelho);
+    sessionStorage.setItem("corPadrao", corVermelho);
+    sessionStorage.setItem("svgPadrao", SVGVERMELHO); 
+    configModal.classList.add("hide");
+
+    for(var i = 0; i < icones_svg.length; i++) {
+    icones_svg[i].classList.remove('svg-azul');
+    icones_svg[i].classList.remove('svg-verde');
+    icones_svg[i].classList.add('svg-vermelho');
+    }
 });
---
+++ palavra.js
@@ -0,0 +1,9 @@
+// SELETORES
+var salvarBtn = document.querySelector("#btnPalavraSalvar");
+
+var inputPalavra = document.querySelector(".inputPalavra");
+var palavra = inputPalavra.value;
+var inputGrupo = document.querySelector(".inputGrupo");
+var inputDica = document.querySelector(".inputDica");
+
+
