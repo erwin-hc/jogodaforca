928dafcd8394157895e5a02d780d0060c9b5d585
erwin-hc <erwin.stein@gmail.com>
2022-05-19 16:53:26 -0300

    19/05/22 -- 17:00

--- palavra.js
+++ palavra.js
@@ -8,18 +8,24 @@ var inputDica = document.querySelector(".inputDica");
 salvarBtn.addEventListener("click", function(e){
 	e.preventDefault();
 	
+	if (!isNaN(inputPalavra.value)) {
+		inputPalavra.value = "";
+		inputPalavra.placeholder = "APENAS LETRAS PERMITIDAS.."; 
+		inputPalavra.focus();
+		return false;
+	}
 	// CHECA SE OS INPUTS ESTAO PREENCHIDOS
 	if (inputPalavra.value == "") {
 		inputPalavra.focus();
-		return
+		return false;
 	} else if (inputGrupo.value == ""){
 		inputGrupo.focus();
-		return
+		return false;
 	} else if (inputDica.value == "") {
 		inputDica.focus();
 	} else {
 		// SE TUDO TIVER PREENCHIDO SETA AS VARIAVES NO SESSION STORAGE E LIMPA OS CAMPOS
-		sessionStorage.setItem("palavra", inputPalavra.value.toUpperCase());
+		sessionStorage.setItem("palavra", inputPalavra.value.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ""));
 		sessionStorage.setItem("grupo", inputGrupo.value.toUpperCase());
 		sessionStorage.setItem("dica", inputDica.value.toUpperCase());
 		sessionStorage.setItem("aleatorio", "NAO");
@@ -33,4 +39,4 @@ salvarBtn.addEventListener("click", function(e){
 
 
 
-
+// const removeAcentos = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
